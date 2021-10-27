751d78d83f756c939decc43c204a88102696e176
Ericka Njinguet <erickanjinguet@gmaiil.com>
2021-10-27 13:31:05 -0400

    Working

--- Group_idea.py
+++ Group_idea.py
@@ -1,1 +1,4 @@
-#As a group one of our ideas was to make a surf board rental game.
+#As a group one of our ideas was to make a type of rental where a user will be 
+#prompted to enter the time they want to rent "product" and then this will 
+#make their time run through and based on the time they have inputed, they will 
+# be given an amount they have to pay that correspoinds with said time.
