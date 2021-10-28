ee4b108b4e26da125a0922aa7a892c674eb58f55
Josephine Ekenya <josephineekenya@yahoo.com>
2021-10-28 00:06:56 -0400

    added rental ideas

--- Group_idea.py
+++ Group_idea.py
@@ -2,3 +2,4 @@
 #prompted to enter the time they want to rent "product" and then this will 
 #make their time run through and based on the time they have inputed, they will 
 # be given an amount they have to pay that correspoinds with said time.
+# Rental ideas as of now: car rentals, calculator rentas, beach umbrella rentals...etc
