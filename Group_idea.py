ffed64a56652144b68a71fa6e9d1226cdb9a06df
mdiaby1 <moryd@wireless-10-105-194-52.umd.edu>
2021-10-29 09:18:31 -0400

    mori's edits

--- Group_idea.py
+++ Group_idea.py
@@ -2,5 +2,23 @@
 #prompted to enter the time they want to rent "product" and then this will 
 #make their time run through and based on the time they have inputed, they will 
 # be given an amount they have to pay that correspoinds with said time.
-# Rental ideas as of now: car rentals, calculator rentals, beach umbrella rentals
-# skateboard or roller skate rentals
+# Rental ideas as of now: car rentals, calculator rentas, beach umbrella rentals...etc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#We mainly want to center our idea on a subscription platform of some sort theres still more ideas we can come up with
+# like a companion app that reminds you to do things, or maybe even an app for students in college who want to do laundry that notifies them what
+# laundry machine is not being used or other things 
+# We could also create an app that lets you sync your youtube playlist to your spotify playlist or vice versa#
