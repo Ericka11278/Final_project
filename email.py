f73765f5298409fc47a2cc6c46f7154876942cd2
mdiaby1 <moryd@wireless-10-105-206-231.umd.edu>
2021-11-15 19:19:07 -0500

    Added docstrings

--- email.py
+++ email.py
@@ -1,6 +1,24 @@
 import smtplib, ssl
+"""
+This method is supposed to send an email receipt to the customer, I imported smtplib a built in python module
+to send emails in python. it is a bit fishy because it makes you login to an email account to send emails so I created a sample one
+Also in order to recieve the email you have to turn off a security feature in your google account
 
+
+ Args: 
+    user (str): email adress which is the user name for the sample email
+    passw(str): a password for the sample email
+    sender(str) : user or basically the sample email address im using to send to myself
+    message(str): the message being sent to the user 
+    port(int): the server used in google email system
+"""
 def read_email():
+    """
+    This method separates the user from the password in the txt file and returns it
+    Returns:
+        user and passw from the tt file
+
+    """
     user = passw = ""
     with open("sample_email.txt", "r") as f:
         file = f.readlines()
