c9ca965113bee9f2aa26ec25ad5383ebaef23220
Josephine Ekenya <josephineekenya@yahoo.com>
2021-11-15 22:18:06 -0500

    josephine last changes

--- latefees.py
+++ latefees.py
@@ -3,8 +3,14 @@ from datetime import datetime
 
 def lateFees(self, user_request): 
     """
-    - This method will go through the  items rented out, the date they were rented, the days they were planned/requested to be rented out for by an
-    individual, and will compute a late fee based on the items  request and how late 
+    - This method will go through the  items rented out, the date they were rented, and the days they were planned/requested to be rented out for by an
+    individual, and will compute a late fee based on the items requested and how late they were returned. 
+    
+    Args: 
+      user_requests: 
+               rentLabCoat, rentCalculator, rentGoggles, plannedRendtDyas: all the user's request such as the items requested, the days requested for , and the day requested. 
+    Returns: 
+        returns the late fee for each/all items returned late 
     """
     rentLabCoat, rentCalculator, rentGoggles,rentDate, plannedRentDays = user_request
     
